9ae2d670e9a4ca96259b1a1eca9af7e23a196d4b
aeyonblack <tanyamasvitaog@gmail.com>
2022-04-26 08:19:03 +0200

    added temp conversion methods

--- lib/src/utilities/converters.dart
+++ lib/src/utilities/converters.dart
@@ -6,6 +6,14 @@ class Temperature {
   double? get kelvin => _kelvin;
   double get celsius => _kelvin! - 273.15;
   double get fahrenheit => _kelvin! * (9/5) - 459.67;
+
+  double as(Unit unit) {
+    switch(unit) {
+      case Unit.celsius: return celsius;
+      case Unit.kelvin: return kelvin ?? 0.0;
+      case Unit.fahrenheit: return fahrenheit;
+    }
+  }
 }
 
 enum Unit {
